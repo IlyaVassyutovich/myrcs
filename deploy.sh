f50670e95f138597778e4cfaf3e216ba0a8906b2
Ilya Vassyutovich <ivasyutovich@qsoft.ru>
2013-08-17 02:44:51 +0400

    Удален мусор, исправлена работа с путями

--- deploy.sh
+++ deploy.sh
@@ -6,25 +6,16 @@ echo "================= DEPLOY RCs ================="
 echo "WARNING : Backing up existing configs did not"
 echo "implemented yet. Be carefull!"
 
-#/home/monomah/configs/deploy.sh
-#/etc/apt
-#/home/monomah/configs
-
-RCDIR="`dirname $0`/conf.d"
+RCDIR="`pwd`/conf.d"
 LN=`which ln`
 SYML='-s'
-
-#echo $0
-#echo `pwd`
-#echo `dirname $0`
+DATE=`date +%y%m%d`
 
 cd ~/
-##echo `pwd`
 for RC in `ls -A $RCDIR`
 do
-##    echo $RC
     if [ -r $RC ]; then
-            mv $RC $RC.bak
+            mv $RC $RC.$DATE.bak
     fi
     $LN $SYML $RCDIR/$RC ./$RC
 done
