d560a3e32f38bf4c3adfb33a17c1b3898340e592
Ilya Vassyutovich <ivasyutovich@qsoft.ru>
2013-08-19 12:38:43 +0400

    Исправлена работа с путями в deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,17 @@
 #! /bin/bash
 
-#TODO: Заменить cd ~/ на ln
-
 echo "================= DEPLOY RCs ================="
-echo "WARNING : Backing up existing configs did not"
-echo "implemented yet. Be carefull!"
 
-#/home/monomah/configs/deploy.sh
-#/etc/apt
-#/home/monomah/configs
-
-RCDIR="`dirname $0`/conf.d"
+cd `dirname $0`
+SCRIPTDIR="`pwd -P`"
+RCDIR="$SCRIPTDIR/conf.d"
 LN=`which ln`
 SYML='-s'
 
-#echo $0
-#echo `pwd`
-#echo `dirname $0`
-
-cd ~/
-##echo `pwd`
 for RC in `ls -A $RCDIR`
 do
-##    echo $RC
-    if [ -r $RC ]; then
-            mv $RC $RC.bak
+    if [ -r ~/$RC ]; then
+        mv ~/$RC ~/$RC.`date +%y%m%d`.bak
     fi
-    $LN $SYML $RCDIR/$RC ./$RC
+    $LN $SYML $RCDIR/$RC ~/$RC
 done
