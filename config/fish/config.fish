f9939478418e809349169e998c6550ba6eabf8be
Ilya Vassyutovich <MONOmah_V@mail.ru>
2018-07-09 20:37:32 +0300

    [fish] Добавляю инициализацию tty для gpg-agent

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,3 +4,6 @@ if not set -q abbrs_initialized
     abbr sfish "sudo -E -i /usr/bin/fish"
     abbr ssc "sudo systemctl"
 end
+
+# https://github.com/Homebrew/homebrew-core/issues/14737#issuecomment-309848851
+set --export GPG_TTY (tty)
