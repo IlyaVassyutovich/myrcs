3814e20cf7a6e673e1cf3825a71f1363bc78337c
Ilya Vassyutovich <MONOmah_V@mail.ru>
2019-12-13 23:51:12 +0300

    [fish] Исправляю шорткат sfish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,7 @@
 if not set -q abbrs_initialized
     set -U abbrs_initialized
 
-    abbr sfish "sudo -E -i /usr/bin/fish"
+    abbr sfish "sudo -i /usr/bin/fish"
     abbr ssc "sudo systemctl"
     abbr scr screen -D -R 
     abbr svirsh "sudo virsh"
