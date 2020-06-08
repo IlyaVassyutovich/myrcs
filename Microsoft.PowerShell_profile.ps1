7d9657c4aa8678e6cc301e332ea345d3593b66c1
Ilya Vassyutovich <vasutovich@mindbox.ru>
2020-06-08 14:08:13 +0300

    [posh] Добавляю posh-profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,11 @@
+New-Alias kc kubectl
+New-Alias tf "C:\bin\terraform.exe"
+New-Alias helmfile "C:\bin\helmfile.exe"
+
+$mr = $env:mr
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
