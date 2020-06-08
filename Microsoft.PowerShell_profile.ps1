b20870b2cdb02ccbc6637933bc1183be74838a15
Ilya Vassyutovich <vasutovich@mindbox.ru>
2020-06-08 14:14:46 +0300

    [posh] Добавляю функцию для создания симлинков

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,3 +9,8 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+function New-SymbolicLink ([string] $LinkValue, [string] $TargetPath)
+{
+  New-Item -ItemType SymbolicLink -Verbose -Path $LinkValue -Value $TargetPath
+}
