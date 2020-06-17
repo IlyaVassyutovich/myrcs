4af747dfa5a5e4fff48941e09998f6f66f3a6240
Ilya Vassyutovich <vasutovich@mindbox.ru>
2020-06-17 17:56:40 +0300

    [posh] Добавляю удобняшки для работы с kubectl и логами

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,7 +10,32 @@ if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 
-function New-SymbolicLink ([string] $LinkValue, [string] $TargetPath)
-{
+function New-SymbolicLink ([string] $LinkValue, [string] $TargetPath) {
   New-Item -ItemType SymbolicLink -Verbose -Path $LinkValue -Value $TargetPath
+}
+
+function Get-EffectiveSelector([string] $AppLabel, [string] $Selector) {
+  if (-Not [string]::IsNullOrWhiteSpace($Selector)) {
+    $EffectiveSelector = $Selector
+  }
+  elseif (-Not [string]::IsNullOrWhiteSpace($AppLabel)) {
+    $EffectiveSelector = "app=$AppLabel"
+  }
+  else {
+    throw "Provide selector or app label"
+  }
+
+  return $EffectiveSelector
+}
+
+function Watch-PodLogs ([string] $AppLabel, [string] $Selector) {
+  kubectl logs --follow="true" --selector="$(Get-EffectiveSelector $AppLabel $Selector)" --since="60s"
+  # TODO: implement reconnection, e. g. after deployment rollout
+}
+
+function Get-PodName ([string] $AppLabel, [string] $Selector)
+{
+  $PodName = (kubectl get pod --selector="$(Get-EffectiveSelector $AppLabel $Selector)" --output=json | ConvertFrom-Json).items.metadata.name `
+    | Select-Object -First 1
+  return $PodName
 }
