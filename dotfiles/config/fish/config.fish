86a4e396d534677b40f1f8a035f79309231630c6
Ilya Vassyutovich <MONOmah_V@mail.ru>
2020-09-19 23:46:00 +0000

    [fish] Добавляю шорткат к systemctl

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -3,6 +3,7 @@ if not set -q abbrs_initialized
 
     abbr sfish "sudo -i /usr/bin/fish"
     abbr ssc "sudo systemctl"
+    abbr sc "systemctl"
     abbr scr "screen -D -R"
     abbr svirsh "sudo virsh"
     abbr svim "sudo -E vim"
