6fda12e3fc708d048969a64b024ab5bd372407d8
Ilya Vassyutovich <vassyutovich@mindbox.ru>
2020-10-31 17:37:17 +0300

    [posh] Split mindbox-specific stuff into separate "profile"-file

---
+++ posh/mindbox.ps1
@@ -0,0 +1,12 @@
+$_mr = $env:mr
+
+if ($_mr) {
+	$mindbox = @{
+		root      = $_mr;
+		product   = Join-Path $_mr "DirectCrm";
+		frontend  = Join-Path $_mr "frontend";
+		uikit     = Join-Path $_mr "ui-kit";
+		messaging = Join-Path $_mr "Messaging";
+		wpg       = Join-Path $_mr "WebPushGate"
+	}  
+}
--- posh/profile.ps1
+++ posh/profile.ps1
@@ -2,19 +2,7 @@ New-Alias kc kubectl
 New-Alias tf "C:\bin\terraform.exe"
 New-Alias helmfile "C:\bin\helmfile.exe"
 
-$_mr = $env:mr
-
-if ($_mr) {
-	$mindbox = @{
-		root      = $_mr;
-		product   = Join-Path $_mr "DirectCrm";
-		frontend  = Join-Path $_mr "frontend";
-		uikit     = Join-Path $_mr "ui-kit";
-		messaging = Join-Path $_mr "Messaging";
-		wpg       = Join-Path $_mr "WebPushGate"
-	}  
-}
-
+. (Join-Path $PSScriptRoot mindbox.ps1)
 . (Join-Path $PSScriptRoot chocolatey-profile.ps1)
 . (Join-Path $PSScriptRoot helpers.ps1)
 . (Join-Path $PSScriptRoot prompt.ps1)
