21f8718ca4dff90cca5efd92f58ef5f1310f474e
Ilya Vassyutovich <vassyutovich@mindbox.ru>
2020-12-03 16:20:32 +0300

    [posh] Implement support for posh-git

--- posh/profile.ps1
+++ posh/profile.ps1
@@ -7,3 +7,6 @@ New-Alias helmfile "C:\bin\helmfile.exe"
 . (Join-Path $PSScriptRoot helpers.ps1)
 . (Join-Path $PSScriptRoot prompt.ps1)
 . (Join-Path $PSScriptRoot k8s-helpers.ps1)
+
+Import-Module posh-git
+$GitPromptSettings.PathStatusSeparator = [string]::Empty
--- posh/prompt.ps1
+++ posh/prompt.ps1
@@ -1,7 +1,19 @@
-function Prompt
-{
+function Prompt {
+	$ExitCodeBackup = $LASTEXITCODE
 	$TimeString = Get-Date -Format "HH:MM:ss"
 	$CurrentLocation = $executionContext.SessionState.Path.CurrentLocation
 	$RolePrompt = if (Test-IsAdmin) { "#" } else { "$" }
-	return "[$TimeString] $CurrentLocation $($RolePrompt * ($nestedPromptLevel + 1)) ";
+
+
+	if (Get-GitStatus -Force) {
+		$VcsPrompt = "─$(Write-VcsStatus)"
+	}
+	else {
+		$VcsPrompt = ""
+	}
+
+
+	$LASTEXITCODE = $ExitCodeBackup
+
+	return "┬─[$TimeString]─[$CurrentLocation]$VcsPrompt$([System.Environment]::NewLine)╰─>$($RolePrompt * ($nestedPromptLevel + 1)) ";
 }
