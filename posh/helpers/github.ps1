81c981c648bfd02508bf1d26942f52eecfe7ce90
Ilya Vassyutovich <vassyutovich@mindbox.ru>
2020-12-08 17:21:40 +0300

    [posh] Add helper to open GH repository

--- posh/helpers/github.ps1
+++ posh/helpers/github.ps1
@@ -15,3 +15,20 @@ function New-PullRequest() {
 	Start-Process $NewPullRequestUri
 }
 New-Alias npr "New-PullRequest"
+
+function Open-GitHubRepository() {
+	$GitStatus = Get-GitStatus -Force
+	if ($null -EQ $GitStatus) {
+		throw "Not in git repo."
+	}
+
+	$GitHubSettingsFilePath = "./.git/github.json"
+	if (-not (Test-Path $GitHubSettingsFilePath -PathType Leaf)) {
+		throw "GitHub settings not found."
+	}
+
+	$RepositoryUri = Get-Content $GitHubSettingsFilePath | ConvertFrom-Json | Select-Object -ExpandProperty repositoryUri
+
+	Start-Process $RepositoryUri
+}
+New-Alias oghr "Open-GitHubRepository"
