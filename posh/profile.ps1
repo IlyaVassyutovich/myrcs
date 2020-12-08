96e21452684ea24e70fcb4bb6970db6b1ed8e3d3
Ilya Vassyutovich <vassyutovich@mindbox.ru>
2020-12-08 17:35:30 +0300

    [posh] Extract k8s aliases

--- posh/profile.ps1
+++ posh/profile.ps1
@@ -1,6 +1,3 @@
-New-Alias kc kubectl
-New-Alias tf "C:\bin\terraform.exe"
-New-Alias helmfile "C:\bin\helmfile.exe"
 
 . (Join-Path $PSScriptRoot mindbox.ps1)
 . (Join-Path $PSScriptRoot chocolatey-profile.ps1)
