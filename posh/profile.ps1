e1444a74ac7f6659cc873a09d69c29bf83a1e8e6
Ilya Vassyutovich <vassyutovich@mindbox.ru>
2020-12-08 17:41:47 +0300

    [posh] 🚀 Add alias to quit posh FASTER

--- posh/profile.ps1
+++ posh/profile.ps1
@@ -1,9 +1,11 @@
-
 . (Join-Path $PSScriptRoot mindbox.ps1)
 . (Join-Path $PSScriptRoot chocolatey-profile.ps1)
 . (Join-Path $PSScriptRoot helpers.ps1)
 . (Join-Path $PSScriptRoot prompt.ps1)
 . (Join-Path $PSScriptRoot k8s-helpers.ps1)
+
+function Exit-CurrentSession { exit }
+New-Alias $([char]4) Exit-CurrentSession
 
 Import-Module posh-git
 $GitPromptSettings.PathStatusSeparator = [string]::Empty
