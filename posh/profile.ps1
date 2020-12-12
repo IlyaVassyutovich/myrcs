17e4693abae127f4b7bc301335a804635284eb3d
Ilya Vassyutovich <vassyutovich@mindbox.ru>
2020-12-12 20:06:30 +0300

    [posh] Implement conditional profile configuration

--- posh/profile.ps1
+++ posh/profile.ps1
@@ -1,4 +1,11 @@
-. (Join-Path $PSScriptRoot mindbox.ps1)
+Write-Debug "Checking Mindbox work station flag"
+$IsMindboxWorkStation = ${ENV:mindbox/isMindboxWorkStation}
+Write-Debug "Mindbox Work station flag is $IsMindboxWorkStation"
+if ($IsMindboxWorkStation -eq $True) {
+	Write-Debug "Evaluating Mindbox-specific settings"
+	. (Join-Path $PSScriptRoot mindbox.ps1)
+}
+
 . (Join-Path $PSScriptRoot chocolatey-profile.ps1)
 . (Join-Path $PSScriptRoot helpers.ps1)
 . (Join-Path $PSScriptRoot prompt.ps1)
