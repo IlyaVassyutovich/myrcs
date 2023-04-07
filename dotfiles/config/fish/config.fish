fbf2e1c5e3e6b01693b1365383648e6a471c161a
Ilya Vassyutovich <me@iv.link>
2023-04-07 21:59:25 +0300

    [fish] Rework abbreviations initialization

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,23 +1,22 @@
-if not set -q abbrs_initialized
-    set -U abbrs_initialized
+abbr sfish "sudo -i /usr/bin/fish"
+abbr ssc "sudo systemctl"
+abbr sc "systemctl"
+abbr scr "screen -D -R"
+abbr svirsh "sudo virsh"
+abbr svim "sudo -E vim"
+abbr megacli "sudo MegaCli64"
+abbr siotop "sudo iotop --only --processes"
+abbr siftop "sudo iftop -n -N -B"
 
-    abbr sfish "sudo -i /usr/bin/fish"
-    abbr ssc "sudo systemctl"
-    abbr sc "systemctl"
-    abbr scr "screen -D -R"
-    abbr svirsh "sudo virsh"
-    abbr svim "sudo -E vim"
-    abbr megacli "sudo MegaCli64"
-    abbr siotop "sudo iotop --only --processes"
-    abbr siftop "sudo iftop -n -N -B"
+abbr d "docker"
+abbr dc "docker compose"
 
-    abbr d "docker"
-    abbr dc "docker compose"
-end
 
 # https://github.com/Homebrew/homebrew-core/issues/14737#issuecomment-309848851
 set --export GPG_TTY (tty)
 
+
 set --export LESS R
+
 
 set fish_greeting
